5916a8b0cd1d637ddc8c1ea539fe890b48a1e159
xiaowen1231 <shiao1124@gmail.com>
2023-07-20 20:38:28 +0800

    20230720

--- prjOniqueWebsite/Models/Repositories/ProductRepository.cs
+++ prjOniqueWebsite/Models/Repositories/ProductRepository.cs
@@ -6,10 +6,24 @@ namespace prjOniqueWebsite.Models.Repositories
     public class ProductRepository
     {
         private readonly OniqueContext _context;
+
         public ProductRepository(OniqueContext context)
         {
             _context = context;
         }
 
+        public List<ProductCardDto> NewArrivalsTop4()
+        {
+            var query = _context.Products
+                .OrderByDescending(p => p.AddedTime)
+                .Take(4)
+                .Select(p => new ProductCardDto
+                {
+                    ProductName = p.ProductName,
+                    Price = p.Price,
+                    PhotoPath = p.PhotoPath
+                });
+            return query.ToList();
+        }
     }
 }
