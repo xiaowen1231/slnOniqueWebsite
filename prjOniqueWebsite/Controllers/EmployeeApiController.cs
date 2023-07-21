86da4af59c9775fff7009e14c6289de0ad8f8570
EricYang0520 <131692920+EricYang0520@users.noreply.github.com>
2023-07-21 17:22:57 +0800

    20230721

--- prjOniqueWebsite/Controllers/EmployeeApiController.cs
+++ prjOniqueWebsite/Controllers/EmployeeApiController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using prjOniqueWebsite.Models.DTOs;
 using prjOniqueWebsite.Models.EFModels;
 
 namespace prjOniqueWebsite.Controllers
@@ -13,6 +14,27 @@ namespace prjOniqueWebsite.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult ShowEmployeeList()
+        {
+            var query = from e in _context.Employees
+                        join el in _context.EmployeeLevel
+                        on e.Position equals el.EmployeeLevelId
+                        select new EmployeeListDto 
+                        {
+                            EmployeeId = e.EmployeeId,
+                            PhotoPath = e.PhotoPath,
+                            EmployeeName = e.EmployeeName,
+                            DateOfBirth = (DateTime)e.DateOfBirth,
+                            Gender = e.Gender ? "女" : "男",
+                            Phone = e.Phone,
+                            Email = e.Email,
+                            EmployeeLevelName = el.EmployeeLevelName
+                        };
+
+            List<EmployeeListDto> dto = query.ToList();
+            return Json(dto);
         }
     }
 }
