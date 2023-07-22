f4b7993b90e3b79f7974dc778ae3429620b44d22
Jimmychang06 <129496213+Jimmychang06@users.noreply.github.com>
2023-07-23 01:11:53 +0800

    20230723

--- prjOniqueWebsite/Controllers/MemberApiController.cs
+++ prjOniqueWebsite/Controllers/MemberApiController.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using prjOniqueWebsite.Models.EFModels;
 
 namespace prjOniqueWebsite.Controllers
 {
     public class MemberApiController : Controller
     {
+        private readonly OniqueContext _context;
+        public MemberApiController(OniqueContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
