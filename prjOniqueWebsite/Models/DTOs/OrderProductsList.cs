b4a4f355d6ab853e795e65e58bf748b9efed967f
allium5566 <j81103@gmail.com>
2023-07-22 16:53:29 +0800

    loadOrderProductList

---
+++ prjOniqueWebsite/Models/DTOs/OrderProductsList.cs
@@ -0,0 +1,12 @@
+﻿namespace prjOniqueWebsite.Models.DTOs
+{
+    public class OrderProductsList
+    {
+        public string ProductName { get; set; }
+        public int OrderQuantity { get; set; }
+        public decimal Price { get; set; }
+        public string SizeName { get; set; }
+        public string ColorName { get; set; }
+        public string PhotoPath { get;set; }
+    }
+}
