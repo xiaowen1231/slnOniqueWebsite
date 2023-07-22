736127670c5c3274f0c0b6e729e1f189a3ee938f
xiaowen1231 <131440008+xiaowen1231@users.noreply.github.com>
2023-07-23 00:39:43 +0800

    20230723

--- prjOniqueWebsite/Models/Daos/ProductDao.cs
+++ prjOniqueWebsite/Models/Daos/ProductDao.cs
@@ -1,4 +1,5 @@
-﻿using prjOniqueWebsite.Models.DTOs;
+﻿using prjOniqueWebsite.Models.Dtos;
+using prjOniqueWebsite.Models.DTOs;
 using prjOniqueWebsite.Models.EFModels;
 
 namespace prjOniqueWebsite.Models.Repositories
@@ -19,6 +20,7 @@ namespace prjOniqueWebsite.Models.Repositories
                 .Take(4)
                 .Select(p => new ProductCardDto
                 {
+                    Id = p.ProductId,
                     ProductName = p.ProductName,
                     Price = p.Price,
                     PhotoPath = p.PhotoPath
@@ -34,16 +36,43 @@ namespace prjOniqueWebsite.Models.Repositories
                          on p.ProductId equals psd.ProductId
                          join od in _context.OrderDetails
                          on psd.StockId equals od.StockId
-                         group od by new { p.Price, p.ProductName, p.PhotoPath } into grouped
+                         group od by new { p.Price, p.ProductName, p.PhotoPath ,p.ProductId} into grouped
                          orderby grouped.Sum(od => od.OrderQuantity) descending
                          select new ProductCardDto
                          {
+                             Id = grouped.Key.ProductId,
                              ProductName = grouped.Key.ProductName,
                              Price = grouped.Key.Price,
                              PhotoPath = grouped.Key.PhotoPath
                          }).Take(4);
 
             return query.ToList();
+        }
+
+        public ProductDetailDto GetProductDetail(int id)
+        {
+            
+            ProductDetailDto dto = new ProductDetailDto();
+
+            dto.products = _context.Products.FirstOrDefault(p => p.ProductId == id);
+
+            if (dto.products == null)
+                throw new Exception("請確認產品名稱");
+
+            dto.PSD_List = _context.ProductStockDetails.Where(psd => psd.ProductId == dto.products.ProductId).ToList();
+
+            if (dto.PSD_List.Count == 0)
+                throw new Exception("請先新增庫存資訊");
+
+            foreach (var item in dto.PSD_List)
+            {
+                var color = _context.ProductColors.FirstOrDefault(c => c.ColorId == item.ColorId);
+                if(!dto.Color.Any(c=>c.ColorId==item.ColorId))
+                dto.Color.Add(color);
+                var size = _context.ProductSizes.FirstOrDefault(s => s.SizeId == item.SizeId);
+                dto.Size.Add(size);
+            }
+            return dto;
         }
     }
 }
