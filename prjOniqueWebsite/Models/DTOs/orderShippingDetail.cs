dad07f7cf03df4fefc5e036231e8dc9d3fab4923
allium5566 <j81103@gmail.com>
2023-07-22 23:33:13 +0800

    7/22
    7/22
    要如何抓到傳入ㄉ參數?

---
+++ prjOniqueWebsite/Models/DTOs/orderShippingDetail.cs
@@ -0,0 +1,18 @@
+﻿namespace prjOniqueWebsite.Models.DTOs
+{
+    public class orderShippingDetail
+    {
+        public int OrderId { get; set; }
+        public string Name { get; set; }
+        public string PhotoPath { get; set; }
+        public string MethodName { get; set; }
+        public string StatusName { get; set; }
+        public string ShippingAddress { get; set; }
+        public string Phone { get; set; }
+        public string PaymentMethodName { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public DateTime? ShippingDate { get;set; }
+        public DateTime? CompletionDate { get; set; }
+ 
+    }
+}
