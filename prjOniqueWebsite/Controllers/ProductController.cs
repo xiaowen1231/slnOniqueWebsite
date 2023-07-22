7d5f81b758c2c17c23a62eb9e6b31504134c1393
xiaowen1231 <131440008+xiaowen1231@users.noreply.github.com>
2023-07-22 20:24:27 +0800

    20230722

---
+++ prjOniqueWebsite/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prjOniqueWebsite.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
---
+++ prjOniqueWebsite/Models/Dtos/ProductDetailDto.cs
@@ -0,0 +1,12 @@
+﻿namespace prjOniqueWebsite.Models.Dtos
+{
+    public class ProductDetailDto
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public List<string> Color { get; set; }
+        public List<string> Size { get; set; }
+        public int Quantity { get; set; }
+        public int StockQuantity { get; set; }
+    }
+}
