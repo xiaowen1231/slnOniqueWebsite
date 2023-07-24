3a6db73e60a221eef1d166b28c502a756f3e8681
Jimmychang06 <129496213+Jimmychang06@users.noreply.github.com>
2023-07-24 17:57:36 +0800

    20230724

---
+++ prjOniqueWebsite/Models/ViewModels/EditMemberVM.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace prjOniqueWebsite.Models.ViewModels
+{
+    public class EditMemberVM
+    {
+        [Required]
+        public int MemberId { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Gender { get; set; }
+        public string Citys { get; set; }
+        public string Areas { get; set; }
+        public string Address { get; set; }
+        public string MemberLevel { get; set; }
+        public DateTime? RegisterDate { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
