c54585391ce561c77343f0ed79da3e220d819ba8
EricYang0520 <131692920+EricYang0520@users.noreply.github.com>
2023-07-24 11:58:20 +0800

    20230724

---
+++ prjOniqueWebsite/Models/DTOs/EmployeeDto.cs
@@ -0,0 +1,21 @@
+﻿using prjOniqueWebsite.Models.EFModels;
+
+namespace prjOniqueWebsite.Models.DTOs
+{
+    public class EmployeeDto
+    {
+        public string EmployeeName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string EmployeeLevelName { get; set; }
+        public DateTime RegisterDate { get; set; }
+
+        public virtual Areas AreasNavigation { get; set; }
+        public virtual Citys CitysNavigation { get; set; }
+
+        public string Address { get; set; }
+    }
+}
