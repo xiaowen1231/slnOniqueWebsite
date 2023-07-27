b6062e473a1e4a1244c3c68457386b70915eaf2a
xiao <shiao1124@gmail.com>
2023-07-27 21:00:28 +0800

    20230727 購物車開發

--- prjOniqueWebsite/Controllers/ShoppingCartController.cs
+++ prjOniqueWebsite/Controllers/ShoppingCartController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using prjOniqueWebsite.Models.Infra;
 
 namespace prjOniqueWebsite.Controllers
 {
     public class ShoppingCartController : Controller
     {
+        [TypeFilter(typeof(MemberVerify))]
         public IActionResult Index()
         {
             return View();
---
+++ prjOniqueWebsite/Models/Infra/MemberVerify.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace prjOniqueWebsite.Models.Infra
+{
+    public class MemberVerify : IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var httpContext = context.HttpContext;
+            if (!httpContext.Session.Keys.Contains("Login"))
+            {
+                context.Result = new RedirectToActionResult("Login", "Home", null);
+            }
+        }
+    }
+}
