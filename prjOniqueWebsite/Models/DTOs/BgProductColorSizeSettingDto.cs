62f9a65e3c5200a34672743e7cd325b528cf0fda
HaoWei <jasper210102@gmail.com>
2023-07-28 14:11:03 +0800

    20230728

--- prjOniqueWebsite/Models/DTOs/BgProductColorSizeSettingDto.cs
+++ prjOniqueWebsite/Models/DTOs/BgProductColorSizeSettingDto.cs
@@ -3,11 +3,8 @@
     public class BgProductColorSizeSettingDto
     {
         public int ProductId { get; set; }
-        public string ProductName { get; set; }
-        public int StockId { get ; set; }   
-        public int ColorId { get; set; }
-        public string ColorName { get; set; }
-        public int SizeId { get; set; }
+        public string ProductName { get; set; }        
+        public string ColorName { get; set; }        
         public string SizeName { get; set; }
 
     }
