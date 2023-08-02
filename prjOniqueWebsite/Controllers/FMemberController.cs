b8f8d85da1e5bf44357c83e2e3d0dd6e61e9c8b7
LiChun <jimhw1998@icloud.com>
2023-08-02 18:23:05 +0800

    20230802

--- prjOniqueWebsite/Controllers/FMemberController.cs
+++ prjOniqueWebsite/Controllers/FMemberController.cs
@@ -4,9 +4,26 @@ namespace prjOniqueWebsite.Controllers
 {
     public class FMemberController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index( string display)
         {
+            ViewBag.Display = display;
             return View();
+        }
+        public IActionResult MemberInfo()
+        {
+            return PartialView();
+        }
+        public IActionResult MemberOrder()
+        {
+            return PartialView();
+        }
+        public IActionResult MemberMyKeep()
+        {
+            return PartialView();
+        }
+        public IActionResult MemberPassword()
+        {
+            return PartialView();
         }
     }
 }
