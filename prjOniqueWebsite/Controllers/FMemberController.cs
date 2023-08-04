565d164ba54a43edb76db781b77964033576b015
Jimmychang06 <jimhw1998@icloud.com>
2023-08-04 15:15:45 +0800

    20230804

--- prjOniqueWebsite/Controllers/FMemberController.cs
+++ prjOniqueWebsite/Controllers/FMemberController.cs
@@ -1,16 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using prjOniqueWebsite.Models.EFModels;
+using prjOniqueWebsite.Models.Infra;
+using System.Text.Json;
 
 namespace prjOniqueWebsite.Controllers
 {
     public class FMemberController : Controller
     {
+        private readonly OniqueContext _context;
+        public FMemberController(OniqueContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index( string display)
         {
             ViewBag.Display = display;
             return View();
         }
+        [TypeFilter(typeof(MemberVerify))]
         public IActionResult MemberInfo()
         {
+            string json = HttpContext.Session.GetString("Login");
+            Members member = JsonSerializer.Deserialize<Members>(json);
+            var mem = (from m in _context.Members
+                       join 
+                       )
             return PartialView();
         }
         public IActionResult MemberOrder()
---
+++ prjOniqueWebsite/Models/DTOs/FMemberDto.cs
@@ -0,0 +1,16 @@
+﻿namespace prjOniqueWebsite.Models.DTOs
+{
+    public class FMemberDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Gender { get; set; }
+        public string Citys { get; set; }
+        public string Areas { get; set; }
+        public string? Address { get; set; }
+
+    }
+}
