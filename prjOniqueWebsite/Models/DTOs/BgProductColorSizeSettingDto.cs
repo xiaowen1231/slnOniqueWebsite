9fc7c21ffd2d11bce4860186fe852d0c5ef240fa
HaoWei <jasper210102@gmail.com>
2023-08-07 15:04:41 +0800

    20230807

--- prjOniqueWebsite/Models/DTOs/BgProductColorSizeSettingDto.cs
+++ prjOniqueWebsite/Models/DTOs/BgProductColorSizeSettingDto.cs
@@ -1,13 +1,14 @@
-﻿namespace prjOniqueWebsite.Models.DTOs
+﻿using prjOniqueWebsite.Models.EFModels;
+
+namespace prjOniqueWebsite.Models.DTOs
 {
     public class BgProductColorSizeSettingDto
     {
         public int ProductId { get; set; }
-        public string ProductName { get; set; } 
-        public int ColorId { get; set; }
-        public int SizeId { get; set; }
-        public string ColorName { get; set; }        
-        public string SizeName { get; set; }
+        public string ProductName { get; set; }
+        public List<ProductSizes> ProductSizes { get; set; }
+        public List<ProductColors> ProductColors { get; set; }
+
 
     }
 }
