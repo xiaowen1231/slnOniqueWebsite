066ea178a8e9cb0d64f3c8137dd987a93684ad21
HaoWei <jasper210102@gmail.com>
2023-08-10 11:53:49 +0800

    20230810
    分頁可用

---
+++ prjOniqueWebsite/Models/ViewModels/BgProductsPagingVM.cs
@@ -0,0 +1,20 @@
+﻿using prjOniqueWebsite.Models.EFModels;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+namespace prjOniqueWebsite.Models.ViewModels
+{
+    public class BgProductsPagingVM
+    {
+        public List<Products> Products { get; set; }
+
+        public PagingInfo PagingInfo { get; set; }
+    }
+    public class PagingInfo
+    {
+        public int TotalItems { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int CurrenPage { get; set; }
+        public int TotalPages {get; set; }
+    }
+   
+}
