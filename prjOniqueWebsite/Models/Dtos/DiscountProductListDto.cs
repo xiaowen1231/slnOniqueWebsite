e02dd9704eb67308f8698a5895a376c1f4e7583c
xiao <shiao1124@gmail.com>
2023-08-12 09:13:07 +0800

    Update DiscountProductListDto.cs

--- prjOniqueWebsite/Models/Dtos/DiscountProductListDto.cs
+++ prjOniqueWebsite/Models/Dtos/DiscountProductListDto.cs
@@ -2,5 +2,17 @@
 {
     public class DiscountProductListDto
     {
+        public int ProductId { get; set; }
+        public string PhotoPath { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public decimal DiscountMethod { get; set; }
+        public decimal DiscountPrice
+        {
+            get
+            {
+                return this.Price * this.DiscountMethod;
+            }
+        }
     }
 }
