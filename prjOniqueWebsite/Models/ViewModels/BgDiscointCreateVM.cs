2a8b74a66bac6d874a9abe90a7be4301ef31271a
xiaowen1231 <131440008+xiaowen1231@users.noreply.github.com>
2023-08-13 22:54:53 +0800

    20230813

--- prjOniqueWebsite/Models/ViewModels/BgDiscointCreateVM.cs
+++ prjOniqueWebsite/Models/ViewModels/BgDiscointCreateVM.cs
@@ -19,15 +19,15 @@ namespace prjOniqueWebsite.Models.ViewModels
         [Required(ErrorMessage = "折扣不可為空白")]
         [RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "{0}必須為數字")]
         [Range(0.1,0.9,ErrorMessage ="必須為小數0.1至0.9")]
-        public decimal DiscountMethod { get; set; }
+        public decimal? DiscountMethod { get; set; }
 
         [Display(Name = "生效日期")]
         [Required(ErrorMessage = "生效日期不可為空白")]
-        public DateTime BeginDate { get; set; }
+        public DateTime? BeginDate { get; set; } = DateTime.Now;  
 
         [Display(Name = "結束日期")]
         [Required(ErrorMessage = "結束日期不可為空白")]
-        public DateTime EndDate { get; set; }
+        public DateTime? EndDate { get; set; } = DateTime.Now.AddDays(7); 
 
         public string? PhotoPath { get; set; }
         public IFormFile? Photo { get; set; }
